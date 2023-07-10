bf361c51e90e0bf751c2a0331f9bbe08aecdf836
StephanStansfield <StephanStansfield@users.noreply.github.com>
2023-07-09 21:07:16 -0400

    cleaned up code syntax and commenting

--- cam_class.py
+++ cam_class.py
@@ -21,38 +21,36 @@ class CamGeneration:
     def __init__(self, gear_ratios, initial_gr, sit_angle=np.pi):
         '''
         points: number of points to define on cams
-        gear_ratios: points*2 np array       [gear_ratios; angles]
-        initial_gr: scalar, e.g. 0.5    overall gear ratio inner-to-outer
+        gear_ratios: points*2 np array          [gear_ratios; angles]
+        initial_gr: scalar, e.g. 0.5            overall gear ratio inner-to-outer
         Initial guess of inner and outer radii will be 1 and 1/initial_gr, e.g. 1 and 2
         '''
         self.gear_ratios = gear_ratios
-        self.gear_ratios[:, 1] = self.gear_ratios[:, 1]*sit_angle/np.pi # normalize second column of gear ratios to ratio of sit angle to 180 deg
+        self.gear_ratios[:, 1] = self.gear_ratios[:, 1] * sit_angle / np.pi # normalize second row of gear ratios to ratio of sit angle to 180 deg
         self.initial_gr = initial_gr
         self.points = len(self.gear_ratios[:, 0]) # number of points to define on each cam
 
         self.cam_radii = np.ones((self.points, 2)) @ np.diag([1, 1/initial_gr])  # initial guess for inner and outer cam radii
 
         # assuming equal distribution of angles for gear ratios
-        self.input_angles = self.gear_ratios[:, 1] # take the second column of gear ratios to be the input angles
+        self.input_angles = self.gear_ratios[:, 1] # take the second row of gear ratios to be the input angles
         self.ninterp = 360 # number of radii points to interpolate between
-        # self.sit_angle = sit_angle
-        self.sit_ind = round(sit_angle/2/np.pi*self.ninterp) # index of sitting position (if ninterp=360, this will also equal the angle along the cam corresponding to sitting)
+        self.sit_ind = round(sit_angle / 2 / np.pi * self.ninterp) # index of sitting position (if ninterp=360, this will also equal the angle along the cam corresponding to sitting)
 
-        self.angles = np.arange(0, self.ninterp+1, 1)/self.ninterp*2*np.pi # create ndarray of radian angles in range [0, ninterp] 
+        self.angles = np.arange(0, self.ninterp + 1, 1) / self.ninterp * 2 * np.pi # create ndarray of radian angles in range [0, ninterp] 
 
         self.inner_pts = None
         self.outer_pts = None
         
     def calculate_cam_radii(self, kind='cubic'):
         '''
-        Calculates the points on a single cam
-        given the gear ratios.
+        Calculates the points on a single cam given the gear ratios.
         '''
         r_lim = 0.5        # after interpolation, the r_min will be lower than r_lim
         for i in range(self.points): # for all the defined points on a cam
             m = self.gear_ratios[i, 0] / self.initial_gr # normalize input to initial gear ratio
-            r = np.sqrt(m) * self.cam_radii[i, 0]
-            R = self.cam_radii[i, 1] / np.sqrt(m)
+            r = np.sqrt(m) * self.cam_radii[i, 0] # smaller cam radii
+            R = self.cam_radii[i, 1] / np.sqrt(m) # larger cam radii
             if r < r_lim:
                 r = r_lim * self.cam_radii[i, 0]
                 R = r_lim * self.cam_radii[i, 1] / m
@@ -60,7 +58,7 @@ class CamGeneration:
             self.cam_radii[i, 1] = R
 
         self.cam_radii = np.append(self.cam_radii, self.cam_radii[:2, :], axis=0)
-        self.input_angles = np.append(self.input_angles, 2*np.pi+self.input_angles[0:2])
+        self.input_angles = np.append(self.input_angles, 2 * np.pi + self.input_angles[0:2])
 
         # after generating the points for each cam, interpolate spline curves between them
         self.interpolate(kind=kind)
@@ -85,15 +83,15 @@ class CamGeneration:
         R = self.cam_radii[:, 1]
         x_c = np.cumsum(r * 2 * np.pi / self.ninterp) # cumulative arc length at each interpolation point along inner cam
 
-        # scale cam size to desired stroke length
+        # scale cam size to desired stroke length (amount of cable displacement from sitting down)
         ratio = stroke / x_c[self.sit_ind]
         r = r * ratio
         R = R * ratio
-        self.inner_pts = np.array([r*np.cos(self.angles), r*np.sin(self.angles)]).T
-        self.outer_pts = np.array([R*np.cos(self.angles-np.pi/2), R*np.sin(self.angles-np.pi/2)]).T # apply phase change to the outer cam
+        self.inner_pts = np.array([r * np.cos(self.angles), r * np.sin(self.angles)]).T
+        self.outer_pts = np.array([R * np.cos(self.angles - np.pi / 2), R * np.sin(self.angles - np.pi / 2)]).T # apply phase change to the outer cam
         if convex:
             self.convex_cam_pts()
-            self.plot_cams() # why isn't this called with stroke as an input?
+            self.plot_cams()
         return self.inner_pts, self.outer_pts
 
     def convex_cam_pts(self):
@@ -102,8 +100,8 @@ class CamGeneration:
         self.inner_pts = np.vstack((self.inner_pts[inner_hull.vertices, 0], self.inner_pts[inner_hull.vertices, 1])).T
         self.outer_pts = np.vstack((self.outer_pts[outer_hull.vertices, 0], self.outer_pts[outer_hull.vertices, 1])).T
 
-        plt.plot(self.inner_pts[:,0], self.inner_pts[:,1], lw=2)
-        plt.plot(self.outer_pts[:,0], self.outer_pts[:,1], lw=2)
+        plt.plot(self.inner_pts[:,0], self.inner_pts[:,1], lw = 2)
+        plt.plot(self.outer_pts[:,0], self.outer_pts[:,1], lw = 2)
         plt.axis('equal')
         plt.show()
 
@@ -126,8 +124,7 @@ class CamGeneration:
 
     def plot_cams(self, stroke=np.pi):
         '''
-        Plots the cam points as a sanity check
-        feature.
+        Plots the cam points as a sanity check feature.
         stroke: scalar, full stroke of the pulling cable
         '''
 
@@ -139,14 +136,11 @@ class CamGeneration:
         ax.plot(self.angles - np.pi / 2, R * ratio)  # apply phase change to the outer cam
         ax.plot(self.angles, r*ratio)
         ax.set_rticks([2, 4, 6, 8, 10])
-        ax.set_rlabel_position(-22.5)  # Move radial labels away from plotted line
+        ax.set_rlabel_position(-22.5)  # move radial labels away from plotted line
         ax.grid(True)
 
         ax.set_title(f"Cam Demonstration, min inner radius={r.min()*ratio}, max outer radius={R.max()*ratio}", va='bottom')
 
-        # print("r: ", r)
-        # print("x_c: ", x_c)
-        # print("length of r: ", len(r))
         print("sit index: ", self.sit_ind)
         print("x_c[sit_ind]: ", x_c[self.sit_ind])
         print("ratio: ", ratio)
@@ -206,9 +200,9 @@ class CamGeneration:
             plt.ylabel('torque (N*m)')
         plt.show()
 
-    def plot_forces_percentage(self, file, torque=False, stroke=2*np.pi):
+    def plot_forces_percentage(self, file, torque=False, stroke = 2 * np.pi):
         '''
-        Plot the force profiles vs stance percentage
+        Plot the force profiles vs *stance percentage*
         given the gear ratios and solved cam radii.
         stroke: scalar, full stroke of the pulling cable
         '''
@@ -219,8 +213,6 @@ class CamGeneration:
 
         # scale cable displacement and cam radii to match stroke length
         x_c = np.cumsum(r * 2 * np.pi / self.ninterp)       # cable displacement array over entire cam rotation
-        # DEBUG
-        # print("Raw x_c array: ", x_c)
         ratio = stroke / x_c[self.sit_ind]                  # ratio of stroke length to unscaled displacement at sitting index
         print("stroke: ", stroke)
         print("raw sitting displacement, x_c[self.sit_ind]: ", x_c[self.sit_ind])
@@ -228,9 +220,6 @@ class CamGeneration:
         x_c = x_c * ratio
         r = r * ratio
         R = R * ratio
-
-        # DEBUG
-        # print("Scaled x_c array: ", x_c)
 
         # calculate elastic band tension from displacement
         x_e = np.cumsum(R * 2 * np.pi / self.ninterp) * 2     # elastic band displacement array
@@ -241,17 +230,6 @@ class CamGeneration:
         # calculate cable forces from elastic band tension
         f_c = R * f_e / r # tension on cable is a function of elastic tension and gear ratio
 
-        # DEBUG
-        # print("Shape of x_e: ", np.shape(x_e))
-        # print("Shape of R: ", np.shape(R))
-        # print("Shape of r: ", np.shape(r))
-        # print("Shape of f_c: ", np.shape(f_c))
-        # plt.figure()
-        # plt.plot(x_e, f_e)
-        # plt.xlabel('elastic band displacement (m)')
-        # plt.ylabel('elastic band force (N)')
-        # plt.title('band displacement vs force')
-
         ### account for nonlinear change in knee angle during standing ###
         # load experimental knee data
         knee_array = np.loadtxt(file, delimiter = ',', ndmin = 2)
@@ -259,51 +237,22 @@ class CamGeneration:
         knee_angles_raw = knee_array[:,1]; # corresponding knee angle
 
         # interpolate & extrapolate knee angle array to match size of cam arrays
-        knee_fcn = interpolate.interp1d(percentage_raw, knee_angles_raw, kind='cubic', fill_value='extrapolate')
-        percentage = np.linspace(0, 100, self.sit_ind+1)
+        knee_fcn = interpolate.interp1d(percentage_raw, knee_angles_raw, kind = 'cubic', fill_value = 'extrapolate')
+        percentage = np.linspace(0, 100, self.sit_ind + 1)
         knee_angles = knee_fcn(percentage)
 
         # find linear relationship between stand percentage and cable displacement
         knee_angles_flip = np.flip(knee_angles) # flip knee array to go from standing to sitting
         percentage_flip = np.flip(percentage) # flip percentage stance to correspond to knee angle (standing to sitting)
         xc_knee = x_c[self.sit_ind] * (1 - (knee_angles_flip - np.min(knee_angles_flip)) / np.ptp(knee_angles_flip) ) # scale cable displacement to knee angle (goes from standing to sitting)
-        plt.figure()
-        plt.plot(knee_angles_flip, xc_knee)
-        plt.xlabel('knee angle (degree)')
-        plt.ylabel('cable displacement, xc (m)')
-        plt.title('knee angle and cable displacement linear relationship')
-
         
         # add "negative" stance phase to account for part of cam past sitting index
         perc_neg = np.linspace((self.sit_ind - self.ninterp) * (100 / self.sit_ind), -100 / self.sit_ind, self.ninterp - self.sit_ind)
-        # xc_knee_neg = np.linspace(xc_knee[-1], xc_knee[-1] + (self.ninterp - self.sit_ind) * ((xc_knee[-1] - xc_knee[0]) / self.sit_ind), self.ninterp - self.sit_ind) # works but slope is too steep
         xc_knee_neg = np.linspace(xc_knee[-1], xc_knee[-1] + (self.ninterp - self.sit_ind) * ((xc_knee[-1] - xc_knee[ np.int_(3*self.sit_ind/4) ] ) / (self.sit_ind/4)), self.ninterp - self.sit_ind)
-        
-
-        # DEBUG
-        # print("shape of percentage: ", np.shape(percentage))
-        # print("shape of xc_knee: ", np.shape(xc_knee))
-        # print("PERCENTAGE: ", percentage)
-        # print("XC_KNEE: ", xc_knee)
-        
-        # print("perc_neg: ", perc_neg)
-        # print("shape of percentage neg: ", np.shape(perc_neg))
 
         percentage = np.concatenate((perc_neg, percentage))
         percentage_flip = np.flip(percentage)
-        # xc_knee = np.concatenate((xc_knee_neg, xc_knee))
         xc_knee = np.concatenate((xc_knee, xc_knee_neg))
-        
-
-        # print("new percentage: ", percentage)
-        # print("new xc_knee: ", xc_knee)
-        # print("shape of new perc: ", np.shape(percentage))
-        # print("shape of new xc_knee: ", np.shape(xc_knee))
-
-        plt.figure()
-        plt.plot(percentage_flip, xc_knee)
-        plt.xlabel('Stance Percentage (%)')
-        plt.ylabel('Cable Displacement (m)')
         
         # create functions relating cable and elastic displacement to cam angle
         cable_fcn = interpolate.interp1d(x_c, self.angles, kind='cubic', fill_value='extrapolate') # cable displacement in, angle out
@@ -315,50 +264,43 @@ class CamGeneration:
         # calculate elastic displacement from angle
         elastic_percentage = elastic_fcn(angle_percentage)
 
+        # calculate cable force using stance percentage as independent variable
+        f_c_new = R / r * k * elastic_percentage
+        
+        ### plot and save values ###
+        # plot knee angle vs. cable displacement
+        plt.figure()
+        plt.plot(knee_angles_flip, xc_knee)
+        plt.xlabel('knee angle (degree)')
+        plt.ylabel('cable displacement, xc (m)')
+        plt.title('knee angle and cable displacement linear relationship')
+
+        # plot stance percentage vs. cable displacement
+        plt.figure()
+        plt.plot(percentage_flip, xc_knee)
+        plt.xlabel('Stance Percentage (%)')
+        plt.ylabel('Cable Displacement (m)')
+
+        # plot cam angle vs. stance percentage
         plt.figure()
         plt.plot(percentage_flip, angle_percentage)
         plt.xlabel('Stance Percentage (%)')
         plt.ylabel('Cam Angle (rad)')
 
+        # plot elastic band displacement vs. stance percentage
         plt.figure()
         plt.plot(percentage_flip, elastic_percentage)
         plt.xlabel('Stance Percentage (%)')
         plt.ylabel('Elastic displacement (m)')
-
-        # calculate cable force using stance percentage as independent variable
-        # f_c_new = R / r * k * f_e[-1] * (1 - (knee_angles_flip - np.min(knee_angles_flip)) / (np.ptp(knee_angles)))
-        f_c_new = R / r * k * elastic_percentage
-
-        # DEBUG
-        # print("Range of knee angles: ", np.ptp(knee_angles))
-        # print("Flipped knee angle array: ", knee_angles_flip)
-        # print("Normalized flipped knee percentage: ", (knee_angles_flip - np.min(knee_angles_flip)) / (np.ptp(knee_angles)) )
-        # print("max displacement: ", x_c[-1])
-
-        # print("angle_percentage: ", angle_percentage)
-        # print("elastic_percentage: ", elastic_percentage)
-        # print("f_c_new: ", f_c_new)
-
         
-        # plt.figure()
-        # plt.plot(percentage, angle_percentage)
-        # plt.xlabel('Stance Percentage (%)')
-        # plt.ylabel('Cam Angle (rad)')
-        
-        
+        # plot cable force vs. stance percentage
         plt.figure()
-        # plt.plot(self.angles / 2 / np.pi * 360, f_e, label='Elastic Band Force', linewidth=3)
         plt.plot(np.flip(percentage), f_c_new, label='Cable Force', linewidth=3)
         plt.legend(loc="upper right")
         plt.xlabel('Stance Percentage (%)')
-        # plt.xlabel('pulling distance/cam perimeter distance (m)')
         plt.ylabel('Force (N)')
-        # plt.xlim([0, 0.17])
-        # plt.xlim([100-(300/360)*100, 100])
         plt.xlim([-60, 100])
         plt.ylim([0, 250])
-        # plt.gca().set_xticks([0, 0.04, 0.08, 0.12, 0.16])
-        # plt.title("Cable Tension vs. Stance Percentage")
         
         np.savetxt('force_output.csv', np.stack((self.angles, x_c, f_e, f_c), axis=1),  header='angles(rad), pulling distance(m), elastic band force(n), cable force(n)')
         if torque:
@@ -367,10 +309,5 @@ class CamGeneration:
             plt.xlabel('angle (deg)')
             plt.ylabel('torque (N*m)')
         plt.show()
-        
-
-    # def normalize_angles(self, file):
-    #     angle = np.loadtxt(file, delimiter=',')
-    #     print(angle)
 
 
---
+++ create_cams.py
@@ -0,0 +1,37 @@
+"""
+Author: Stephan Stansfield, adapted from code originally by Yuxiang Ma
+Date:   11/14/2022 - 7/9/2023
+Python version 3.10
+"""
+#
+from cam_class import CamGeneration
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import interpolate
+import os
+import sys
+
+# row 0 of gear_ratios holds desired gear ratios
+# row 1 of gear_ratios holds percentage of sitting motion associated with the gear ratios in row 0
+# profiles as of committee meeting 1 (peak at 32.5%; peak force 175 N, sitting force 27.5 N):
+gear_ratios = np.array([[0.18, 0.18, 0.3, 0.5, 1.4,  1.7,  1.5, 0.9],
+                        [0  , 0.08, 0.28, 0.4, 0.45, 0.5, 0.60, 0.66]])
+
+gear_ratios[1, :] = 2 * np.pi * gear_ratios[1, :] # convert row 1 of gear_ratios to radians
+
+initial_gr = 0.5 # initial overall gear ratio, inner cam/outer cam
+
+# generate cams using selected gear ratios, initial gear ratio, and sit angle.
+# sit angle is input in radians.
+Cam = CamGeneration(gear_ratios.T, initial_gr, sit_angle = np.pi * 220 / 180)
+
+Cam.calculate_cam_radii()
+
+# calculate cable force using knee angle data
+filename = 'data/Knee-angle_Chugo_2006.csv'
+Cam.plot_forces_percentage(filename, torque = False, stroke = 0.1)
+
+inner_pts, outer_pts = Cam.cam_pts(stroke=0.1) # output arrays of point coordinates (X, Y, Z) on perimeter of cams
+
+np.savetxt('inner.txt', np.append(inner_pts, np.zeros((inner_pts.shape[0], 1)) * 1000, axis = 1), delimiter = ',')  # mm
+np.savetxt('outer.txt', np.append(outer_pts, np.zeros((outer_pts.shape[0], 1)) * 1000, axis = 1), delimiter = ',')  # mm
